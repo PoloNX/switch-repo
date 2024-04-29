ceced04d9eab08bf90a9fc156031887e771bca14
PoloNX <geyrecharlie@gmail.com>
2024-04-29 20:57:59 +0200

    added simpleiniparser

---
+++ packages/s/simple-ini-parser/xmake.lua
@@ -0,0 +1,31 @@
+package("simple-ini-parser")
+    set_homepage("https://github.com/NicholeMattera/Simple-INI-Parser")
+    set_description("A very simple INI parser written in C++.")
+
+    add_urls("https://github.com/NicholeMattera/Simple-INI-Parser/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/NicholeMattera/Simple-INI-Parser.git")
+
+    add_versions("v2.1.0", "279cb41700df3820d53b80e2509070481bc8e2551db1ae4ac4eb7c5832fb5f7f")
+
+    on_load(function (package)
+        package:add("deps", "libnx")
+    end)
+
+    on_install(function (package)
+        local configs = {}
+        io.writefile("xmake.lua", [[
+            add_rules("mode.release", "mode.debug")
+            target("simple-ini-parser")
+                set_kind("$(kind)")
+                add_files("source/SimpleIniParser/*.cpp")
+                add_headerfiles("include/**.hpp")
+        ]])
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("foo", {includes = "foo.h"}))
+    end)

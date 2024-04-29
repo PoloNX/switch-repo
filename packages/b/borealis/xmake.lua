e502791f1bd00b9f4ee07ff0d152077673b4228a
PoloNX <geyrecharlie@gmail.com>
2024-04-29 17:18:23 +0200

    add deko3d

--- packages/b/borealis/xmake.lua
+++ packages/b/borealis/xmake.lua
@@ -4,17 +4,51 @@ package("borealis")
     set_license("Apache-2.0")
 
     add_urls("https://github.com/PoloNX/borealis.git")
-    add_versions("2024.04.18", "8a9064af864cfeaac0bd0ae306052e68b5302b3a")
+    add_versions("2024.04.18", "ae9b053ce527faaef5356d1acfd4f7a392604451")
 
-    add_deps("cmake")
-
+    add_configs("window", {description = "use window lib", default = "glfw", type = "string"})
+    add_configs("driver", {description = "use driver lib", default = "opengl", type = "string"})
+    add_configs("winrt", {description = "use winrt api", default = false, type = "boolean"})
+    add_deps(
+        "nanovg",
+        "yoga",
+        "nlohmann_json",
+        "fmt",
+        "tweeny",
+        "stb",
+        "tinyxml2"
+    )
+    add_includedirs("include")
+    if is_plat("windows") then
+        add_includedirs("include/compat")
+        add_syslinks("Wlanapi", "iphlpapi", "Ws2_32")
+    end
+    on_load(function (package)
+        local window = package:config("window")
+        local driver = package:config("driver")
+        local winrt = package:config("winrt")
+        if window == "glfw" then
+            package:add("deps", "xfangfang_glfw")
+        elseif window == "sdl" then
+            package:add("deps", "sdl2")
+        end
+        if driver == "opengl" then
+            package:add("deps", "glad")
+        elseif driver == "d3d11" then
+            package:add("syslinks", "d3d11")
+        end
+        if winrt then
+            package:add("syslinks", "windowsapp")
+        end
+    end)
     on_install(function (package)
         local configs = {}
-        table.insert(configs, "-DPLATFORM_SWITCH=ON")
-        os.cd("library")
-        import("package.tools.cmake").install(package, configs)
+        configs["window"] = package:config("window")
+        configs["driver"] = package:config("driver")
+        configs["winrt"] = package:config("winrt") and "y" or "n"
+        import("package.tools.xmake").install(package, configs)
+        os.cp("library/include/*", package:installdir("include").."/")
+        os.rm(package:installdir("include/borealis/extern"))
+        os.cp("library/include/borealis/extern/libretro-common", package:installdir("include").."/")
     end)
-
-    on_test(function (package)
-        assert(package:has_cfuncs("brls::Application::init", {includes = "borealis.hpp"}))
-    end)
+package_end()

f1be5a5e6069a65b02b20800b3fd81ca32ac7552
PoloNX <geyrecharlie@gmail.com>
2024-07-09 15:16:45 +0200

    temp

--- packages/s/switch-pkg/xmake.lua
+++ packages/s/switch-pkg/xmake.lua
@@ -22,6 +22,7 @@ package("switch-pkg")
         elseif os.isexec("pacman") then
             list = os.iorunv("pacman" .. " -Ql " .. pkgname)
         else
+            list = os.iorunv("dkp-pacman" .. " -Ql " .. pkgname)
             cprint("${bright red}Pacman not found: ${reset}%s", pkgname)
         end
 

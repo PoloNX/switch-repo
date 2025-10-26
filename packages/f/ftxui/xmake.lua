056c3382ecb0b07152ae1515d174105074d21f59
PoloNX <geyrecharlie@gmail.com>
2025-10-26 22:38:21 +0100

    test

--- packages/f/ftxui/xmake.lua
+++ packages/f/ftxui/xmake.lua
@@ -11,7 +11,7 @@ package("ftxui")
 
     add_deps("cmake")
 
-    add_patches("5.0.0", "patches/5.0.0/switch.patch", "61e0544177c8146a9e5f2456458274d4f590c0132bb86f6d10108b02525ce04c")
+    -- add_patches("5.0.0", "patches/5.0.0/switch.patch", "61e0544177c8146a9e5f2456458274d4f590c0132bb86f6d10108b02525ce04c")
 
     if is_plat("windows") then
         add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})

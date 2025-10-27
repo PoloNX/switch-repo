d8b21f46c7bfc621e887d007fc81fb86c5408828
PoloNX <geyrecharlie@gmail.com>
2025-10-27 10:32:58 +0100

    test

--- packages/f/ftxui/port/xmake.lua
+++ packages/f/ftxui/port/xmake.lua
@@ -1,10 +1,10 @@
 add_rules("mode.debug", "mode.release")
 
-add_requires("libnx")
+add_requires("libnx", "gtest")
 
 target("ftxui")
     set_kind("$(kind)")
     add_files("src/ftxui/**.cpp")
     add_includedirs("include/ftxui")
     add_includedirs("include")
-    add_packages("libnx")
+    add_packages("libnx", "gtest")
